1230c5a06cec9e67d706697947309d4aad66ab53
katherinesandoval <kate_sandoval27@hotmail.com>
2015-01-27 17:57:50 -0500

    upload

---
+++ Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFactory : MonoBehaviour {
+
+	public float timer;
+	public float minX, maxX;
+	public GameObject enemyObject;
+	private float timeToSpawn;
+
+
+	// Use this for initialization, only runs once
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newX = Random.Range (minX, maxX);
+		transform.position = new Vector3 (newX, 20f, 0);
+
+		if (timeToSpawn < 0) {
+			// Duplicaes objects
+			GameObject g = (GameObject)Instantiate(enemyObject, transform.position, Quaternion.identity);
+			Destroy(g, 5f);
+			timeToSpawn = timer;
+		}
+		timeToSpawn = timeToSpawn - Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	// You can change what keys to use for the user input
+	void Update () {
+		// Debug.Log("Hello World!!");
+		if(Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8){
+			// to keep inside a coordinatre
+			// deltaTime how long the frame took, so it wont run faster on one comp or another
+			transform.position += new Vector3 (10, 0, 0) * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -8){
+			// to keep inside a coordinatre
+			// deltaTime how long the frame took, so it wont run faster on one comp or another
+			transform.position += new Vector3 (-10, 0, 0) * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow) && transform.position.y < 4){
+			// to keep inside a coordinatre
+			// deltaTime how long the frame took, so it wont run faster on one comp or another
+			transform.position += new Vector3 (0, 10, 0) * Time.deltaTime;
+		}
+
+		// smaller number because screen is wider than taller
+		if(Input.GetKey(KeyCode.DownArrow) && transform.position.y > -4){
+			// to keep inside a coordinatre
+			// deltaTime how long the frame took, so it wont run faster on one comp or another
+			transform.position += new Vector3 (0, -10, 0) * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("collision!!!!!!!!");
+		Application.LoadLevel("fin");
+	}
+}
